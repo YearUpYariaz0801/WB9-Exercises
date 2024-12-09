10368dca4286ee9d3aa43285f8659835de9bbff2
Yariaz0801 <designsbyzairay@gmail.com>
2024-12-09 14:52:19 -0500

    Finished Exercise 3

--- NorthwindTradersSpringBoot/src/main/java/com/pluralsight/NorthwindTradersSpringBoot/NorthwindTradersSpringBootApplication.java
+++ NorthwindTradersSpringBoot/src/main/java/com/pluralsight/NorthwindTradersSpringBoot/NorthwindTradersSpringBootApplication.java
@@ -1,7 +1,5 @@
 package com.pluralsight.NorthwindTradersSpringBoot;
 
-import dao.interfaces.ProductDao;
-import models.Product;
 import com.abdurraheem.utils.Console;
 import com.pluralsight.NorthwindTradersSpringBoot.dao.interfaces.ProductDao;
 import com.pluralsight.NorthwindTradersSpringBoot.models.Product;
@@ -18,22 +16,17 @@ public class NorthwindTradersSpringBootApplication {
 	private static ApplicationContext context;
 
 	public static void main(String[] args) {
-
 		context = SpringApplication.run(NorthwindTradersSpringBootApplication.class, args);
-
 		for(String bean : context.getBeanDefinitionNames()){
 			System.out.println(bean);
 		}
-
 		String options = """
                 Please select from the following choices:
                 1 - Add Product
                 2 - List All Products
                 99 - Quit
                 >>>\s""";
-
 		int selection;
-
 		// User Interface Loop
 		do {
 			selection = Console.PromptForInt(options);
@@ -44,7 +37,7 @@ public class NorthwindTradersSpringBootApplication {
 				default -> System.out.println("Invalid selection. Please try again.");
 			}
 		} while (true);
-
+		SpringApplication.run(NorthwindTradersSpringBootApplication.class, args);
 	}
 
 	private static void processGetAllProducts() {
@@ -54,19 +47,13 @@ public class NorthwindTradersSpringBootApplication {
 			System.out.println(p);
 		}
 	}
-
 	private static void processAddProduct() {
 		ProductDao productDao = context.getBean(ProductDao.class);
-
 		int productId = Console.PromptForInt("Please enter the Product ID: ");
 		String name = Console.PromptForString("Please enter the Product Name: ");
 		String category = Console.PromptForString("Please enter the Category:" );
 		double price = Console.PromptForDouble("Please enter the price: ");
 		Product p1 = new Product(productId, name, category, price);
-
 		productDao.add(p1);
-
 	}
-
-
-}
+}}
