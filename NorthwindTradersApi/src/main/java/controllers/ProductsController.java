a4bd7c7576b4d39bf4f6ff2b244144d1ecf913e3
Yariaz0801 <designsbyzairay@gmail.com>
2024-12-10 14:04:57 -0500

    Added Configuration File

--- NorthwindTradersApi/src/main/java/controllers/ProductsController.java
+++ NorthwindTradersApi/src/main/java/controllers/ProductsController.java
@@ -1,12 +1,17 @@
-package com.pluralsight.NorthwindTradersAPI.controllers;
+package controllers;
+
 import models.Product;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.ArrayList;
 import java.util.List;
+
 @RestController
 public class ProductsController {
+
     @RequestMapping(path="/products", method= RequestMethod.GET)
     public List<Product> getProducts(){
         ArrayList<Product> products = new ArrayList<>();
@@ -14,5 +19,15 @@ public class ProductsController {
         products.add(new Product(2, "Apple", 1, 15.99d));
         products.add(new Product(3, "Blue Jeans", 2, 2.99d));
         return products;
+    }
+    @RequestMapping(path="/products/{id}", method= RequestMethod.GET)
+    public Product getProduct(@PathVariable int id){
+        if(id == 1){
+            return new Product(1, "Banana", 1, 5.99d);
+        }
+        else if (id == 2){
+            return new Product(2, "Apple", 1, 15.99d);
+        }
+        return null;
     }
 }
